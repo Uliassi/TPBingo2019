4812b722365761cc28e6de42ce4c447b2f746171
Luis Aseguinolaza <luisasg_2001@hotmail.com>
2019-06-11 17:03:02 -0300

    agregado numeros del carton a clase Carton

--- src/Carton.php
+++ src/Carton.php
@@ -5,31 +5,42 @@ namespace Bingo;
 class Carton {
 			protected $carton = [];
 
-		  public function __construct(array $p_carton) {
-				$this->carton = $p_carton;
+  public function __construct(array $p_carton) {
+		$this->carton = $p_carton;
+  }
+
+  public function filas() {
+		return $this->numeros_carton;
+  }
+
+
+  public function columnas() {
+	   $coltot = [];
+		  $index = 0;
+		  foreach( $this->numeros_carton[$index] as $fila ){
+				$coltot[] = [$this->numeros_carton[0][$index],$this->numeros_carton[1][$index] , $this->numeros_carton[2][$index] ];
+			  $index++;
 		  }
-
-		  public function filas() {
-				return $this->numeros_carton;
-		  }
-
-
-		  public function columnas() {
-			   $coltot = [];
-				  $index = 0;
-				  foreach( $this->numeros_carton[$index] as $fila ){
-						$coltot[] = [$this->numeros_carton[0][$index],$this->numeros_carton[1][$index] , $this->numeros_carton[2][$index] ];
-					  $index++;
-				  }
-				  
-				return $coltot;
-		  }
+		  
+		return $coltot;
+  }
 
 
+  public function numerosDelCarton() {
+    $numeros = [];
+    foreach ($this->filas() as $fila) {
+      foreach ($fila as $celda) {
+        if ($celda != 0) {
+          $numeros[] = $celda;
+        }
+      }
+    }
+    return $numeros;
+  }
 
-		public function tieneNumero(int $numero) {
-				return	in_array($numero, $this->numerosDelCarton());
-		  }
+public function tieneNumero(int $numero) {
+		return	in_array($numero, $this->numerosDelCarton());
+  }
 
 }
 
