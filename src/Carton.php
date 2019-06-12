7cf58a980131ed3b7aafbbb5267831167f27f244
Luis Aseguinolaza <luisasg_2001@hotmail.com>
2019-06-12 14:04:25 -0300

    Arreglando travis

--- src/Carton.php
+++ src/Carton.php
@@ -1,7 +1,8 @@
 <?php
 namespace Bingo;
 include('CartonInterface.php');
-include('FabricaCartones.php'); 
+include('FabricaCartones.php');
+include('CartonEjemplo.php');  
 
 
 class Carton implements CartonInterface {
@@ -46,5 +47,7 @@ public function tieneNumero(int $numero) {
 
 }
 
-//print_r( (new Carton( (new FabricaCartones)->intentoCarton() ) )->columnas());
+print_r( (new Carton( (new FabricaCartones)->intentoCarton() ) )->columnas());
+echo "askjdhakjsdas\n\n";
+print_r( (new CartonEjemplo)->columnas());
 
